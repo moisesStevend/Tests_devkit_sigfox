e8b1ae4b417bc89d4e14bdcfecb0006c425092be
moisesStevend <moises.stevend@gmail.com>
2020-02-05 16:56:43 -0500

    adding function to config battery

--- gluco_mib_sigfox.ino
+++ gluco_mib_sigfox.ino
@@ -1,10 +1,19 @@
+/*********settings varibales for battery*************************/
 int leds_battery[6]={2,3,4,5,6,7};
 
+/*********settings functions as global****************************/
+void battery_indicator_settings(void);
+/**********setup-loop******************************************/
 void setup() {
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+}
+
+/*************functions***************************/
+void battery_indicator_settings(void){
 
 }
